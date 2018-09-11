9c9e72cc523cd385a4e5eea36101bfa7b73ac518
Mariam <mariam.antar@hotmail.com>
2018-09-11 14:42:38 +1000

    edit files

--- helper/auth.js
+++ helper/auth.js
@@ -1,12 +1,12 @@
 module.exports = {
-  // middleware helper function to check if user is logged in or not
+  // middleware helper function to check if the user has logged in or not
   ensureAuthenticated: (req, res, next) => {
-  if (req.isAuthenticated()) {
-    // execute the next function which in most cases would be a route
-    return next();
-  } else {
-    // redirect to Login
-    res.redirect('/users/login')
-    }
+      if (req.isAuthenticated()) {
+        // execute the next function, which in most cases would be route
+        return next();
+      } else {
+        // redirect to login page
+        res.redirect('/users/login');
+      }
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,10 @@ const mongoose = require('mongoose');
 // used to encrypt the password before saving to db
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
-
 // require model
 require('../models/User')
 // create user model
 const User = mongoose.model('users');
-
-
 
 // registration route
 router.get('/register', (req, res) => {
@@ -21,62 +18,55 @@ router.post('/register', (req, res) => {
   // res.send('In the post route');
   console.log(req.body);
   let errors = [];
+
   if(req.body.password.length < 5) {
-    errors.push({text: "password must be at least 5 characters long"})
+    errors.push({text: "Password must be atleast 5 character long"});
   }
   // ensure password matches confirm password
   if(req.body.password != req.body.password2) {
-    errors.push({text: "passwords don't match"})
+    errors.push({text: "Password don't match"});
   }
-  // if any errors re render the sign up page
+  // if there were any errors, re render the signup page and pass the filled in params
   if(errors.length > 0) {
-  res.render('users/register', {
-    name: req.body.name,
-    email: req.body.email ,
-    password: req.body.password ,
-    password2: req.body.password2,
-    errors:errors
-  });
-} else {
-
-
-  // check if email already exists in the db
-
-
-
-  // res.send('passed')
-  // create a user object
-  let newUser = {
-    name: req.body.name,
-    email: req.body.email ,
-    password: req.body.password
-  }
-  // encrypt the password
-  var bcrypt = require('bcryptjs');
-bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(newUser.password, salt, function(err, hash) {
-        // Store hash in your password DB.
-        if (err => console.log(err))
-        // set the new password as the new password in the db
-        newUser.password = hash;
-        // create user model and insert data to database
-        new User(newUser).save()
-        .then(users => {
-          res.redirect('/');
-        })
-        .catch( err => console.log(err))
+    res.render('users/register' , {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2,
+      errors: errors
     });
-});
-}
+  } else {
+    // check if email already exsts in the db
+    // res.send('passed');
+    // create a user object
+      let newUser = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      }
+        // encrypt the password
+      bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+          // Store hash in your password DB.
+          if (err => console.log(err))
+          // set the encrypted password as the new password in the db
+          newUser.password = hash;
+          // create User model and insert data to db
+          new User(newUser).save()
+            .then(users => {
+              // redirect to home page
+                res.redirect('/');
+            })
+            .catch( err => console.log(err));
+      });
+    });
+  }
 })
-
-
 // Login route
 router.get('/login', (req, res) => {
   res.render('users/login');
 });
-
-// post route for Login
+// post route for login
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', {
     successRedirect: '/',
@@ -84,13 +74,11 @@ router.post('/login', (req, res, next) => {
   })(req, res, next);
 })
 
-
 // logout route
 router.get('/logout', (req, res) => {
   req.logout();
   res.redirect('/users/login');
 })
-
 
 
 module.exports = router;
